794d7bafbc94c8c6ac951030d079051ae7df9b4d
Marco von Baumbach <marco.vonbaumbach@gmx.de>
2023-02-08 12:31:02 +0100

    card animation

---
+++ src/models/game.ts
@@ -0,0 +1,30 @@
+export class Game {
+    public players: string[] = [];
+    public stack: string[] = [];
+    public playedCards: string[] = [];
+    public CurrentPlayer: number = 0;
+
+    constructor() {
+        for(let i = 1; i <= 13; i++) {
+            this.stack.push('ace_' + i);
+            this.stack.push('clubs_' + i);
+            this.stack.push('diamonds_' + i);
+            this.stack.push('hearts_' + i);
+        }
+
+        shuffle(this.stack);
+    } 
+}
+
+function shuffle(array: string[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+  
+      // swap elements array[i] and array[j]
+      // we use "destructuring assignment" syntax to achieve that
+      // you'll find more details about that syntax in later chapters
+      // same can be written as:
+      // let t = array[i]; array[i] = array[j]; array[j] = t
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+}
